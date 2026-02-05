ecc51cd990a40f847ef98420720b204de6b31b22
rafaeltoledo94 <engrafaeltoledosouza@gmail.com>
2026-02-05 13:26:56 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,18 +5,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
+      // O truque mágico: diz pro sistema que aquele código estranho é na verdade sua foto
+      'figma:asset/1182ff53dd980e256f26aad516e3cc461abe8f52.png': '/rafael-toledo.png',
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
